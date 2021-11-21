376c1537a9e5c19930a949e7a95365b57c1d7567
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-11-21 13:01:59 +1100

    Added tag routes for create, update and delete.

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -50,14 +50,54 @@ router.get('/:id', (req, res) => {
 
 router.post('/', (req, res) => {
   // create a new tag
+  Tag.create({
+    tag_name: req.body.tag_name
+  })
+    .then(dbTagData => res.json(dbTagData))
+    .catch(err => {
+      console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.put('/:id', (req, res) => {
   // update a tag's name by its `id` value
+  Tag.update({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData[0]) {
+        res.status(404).json({ message: 'No Tag Found With This ID'});
+        return;
+      }
+      res.json(dbTagData);
+    })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 router.delete('/:id', (req, res) => {
   // delete on tag by its `id` value
+  Tag.describe({
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbTagData => {
+      if (!dbTagData) {
+        res.status(404).json({ message: 'No Tag Found WIth This ID'});
+        return;
+      }
+      res.json(dbTagData);
+    })
+      .catch(er => {
+        console.log(err);
+        res.status(500).json(err);
+    });
 });
 
 module.exports = router;
