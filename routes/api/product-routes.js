8b04dde4f003ae9d7987528ef300aeafb91d9a9d
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-11-21 12:48:57 +1100

    Added paramaters for creation of product. Added delete route.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -65,7 +65,13 @@ router.post('/', (req, res) => {
       tagIds: [1, 2, 3, 4]
     }
   */
-  Product.create(req.body)
+  Product.create({
+    product_name: req.body.product_name,
+    price: req.body.price,
+    stock: req.body.stock,
+    category_id: req.body.category_id,
+    tagIds: req.body.tag_id
+  })
     .then((product) => {
       // if there's product tags, we need to create pairings to bulk create in the ProductTag model
       if (req.body.tagIds.length) {
@@ -131,6 +137,22 @@ router.put('/:id', (req, res) => {
 
 router.delete('/:id', (req, res) => {
   // delete one product by its `id` value
+  Product.destroy(req.body, {
+    where: {
+      id: req.params.id
+    }
+  })
+    .then(dbProdData => {
+      if (!dbProdData) {
+        res.status(404).json({message: 'No Products Found With This ID'});
+        return;
+      }
+      res.json(dbProdData);
+    })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 module.exports = router;
