ca541760a36035116bddc0992a7ca5c8cc12bcbe
Luke McMillan <mcmillan.luke@yahoo.com.au>
2021-11-21 12:00:28 +1100

    Added get by id for product route.

--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -30,6 +30,29 @@ router.get('/', (req, res) => {
 router.get('/:id', (req, res) => {
   // find a single product by its `id`
   // be sure to include its associated Category and Tag data
+  Product.findOne({
+    include: [
+      {
+        model: Category,
+        attributes: ['id', 'category_name']
+      },
+      {
+        model: Tag,
+        attributes: ['id', 'tag_name']
+      }
+    ]
+  })
+    .then(dbProdData => {
+      if (!dbProdData) {
+        res.status(404).json({ massage: 'No Product Found With This ID :('})
+        return;
+      }
+      res.json(dbProdData);
+    })
+      .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+      });
 });
 
 // create new product
